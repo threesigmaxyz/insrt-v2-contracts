b358bf325be935cc5745beac9229758040631929
Cruz Molina <cruz.adam.molina@gmail.com>
2023-06-20 15:01:27 -0700

    Feat: Initial Core Diamond contracts (#13)
    * build: rm obsolete .gitkeep
    
    * feat: initial L1 & L2 Core Diamond contracts

---
+++ contracts/diamonds/L1/Core.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.20;
+
+import { SolidStateDiamond } from "@solidstate/contracts/proxy/diamond/SolidStateDiamond.sol";
+
+/// @title L1Core
+/// @dev The L1 Core Diamond.
+contract L1Core is SolidStateDiamond {
+
+}
