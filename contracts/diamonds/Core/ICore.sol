1fda6492bb1d4cc0283292fe9f47c573108e0079
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2023-10-13 08:18:22 -0700

    refactor: ICore inherit IPerpetualMint && IPerpetualMintView

--- contracts/diamonds/Core/ICore.sol
+++ contracts/diamonds/Core/ICore.sol
@@ -3,9 +3,10 @@
 pragma solidity 0.8.19;
 
 import { IPerpetualMint } from "../../facets/PerpetualMint/IPerpetualMint.sol";
+import { IPerpetualMintView } from "../../facets/PerpetualMint/IPerpetualMintView.sol";
 
 /// @title ICore
 /// @dev The Core diamond interface.
-interface ICore is IPerpetualMint {
+interface ICore is IPerpetualMint, IPerpetualMintView {
 
 }
