2fc908942f7630e85d5374fdc5c1b73df10e556b
Cruz Molina <cruz.adam.molina@gmail.com>
2023-11-02 12:17:03 -0700

    build: rework Base VRF configuration for Gnosis Safe integration (#214)

--- script/Base/post-deployment/01_configureVRFSubscription.s.sol
+++ script/Base/post-deployment/01_configureVRFSubscription.s.sol
@@ -1,20 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
-import "forge-std/Script.sol";
+import "forge-safe/BatchScript.sol";
 
 import { IDepositContract } from "../../../contracts/facets/PerpetualMint/Base/IDepositContract.sol";
 import { ISupraRouterContract } from "../../../contracts/facets/PerpetualMint/Base/ISupraRouterContract.sol";
 import { IPerpetualMint } from "../../../contracts/facets/PerpetualMint/IPerpetualMint.sol";
 
 /// @title ConfigureVRFSubscriptionBase
-/// @dev configures the Supra VRF subscription by adding the PerpetualMint contract as a consumer,
-/// and optionally funding the subscription in ETH
-contract ConfigureVRFSubscriptionBase is Script {
+/// @dev Configures the Supra VRF subscription by adding the PerpetualMint contract as a consumer,
+/// and optionally funding the subscription in ETH via the Gnosis Safe Transaction Service API
+contract ConfigureVRFSubscriptionBase is BatchScript {
     /// @dev runs the script logic
     function run() external {
         // get PerpetualMint address
         address perpetualMint = readCoreAddress();
+
+        // get Gnosis Safe (protocol owner) address
+        address gnosisSafeAddress = vm.envAddress("GNOSIS_SAFE");
 
         // get set Supra VRF Router address
         address vrfRouter = readVRFRouterAddress();
@@ -25,32 +28,37 @@ contract ConfigureVRFSubscriptionBase is Script {
         uint256 ethAmountToFundSubscription = envEthAmountToFundSubscription *
             1 ether;
 
-        // read deployer private key
-        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_KEY");
+        address supraVRFDepositContract = ISupraRouterContract(vrfRouter)
+            ._depositContract();
 
-        IDepositContract supraVRFDepositContract = IDepositContract(
-            ISupraRouterContract(vrfRouter)._depositContract()
+        bytes memory addContractToWhitelistTx = abi.encodeWithSelector(
+            IDepositContract.addContractToWhitelist.selector,
+            perpetualMint
         );
 
-        vm.startBroadcast(deployerPrivateKey);
-
-        supraVRFDepositContract.addContractToWhitelist(perpetualMint);
+        addToBatch(supraVRFDepositContract, addContractToWhitelistTx);
 
         if (ethAmountToFundSubscription > 0) {
-            supraVRFDepositContract.depositFundClient{
-                value: ethAmountToFundSubscription
-            }();
+            bytes memory depositFundClientTx = abi.encodeWithSelector(
+                IDepositContract.depositFundClient.selector
+            );
+
+            addToBatch(
+                supraVRFDepositContract,
+                ethAmountToFundSubscription,
+                depositFundClientTx
+            );
         }
 
-        console.log("Supra VRF Router Address: ", vrfRouter);
-        console.log("Supra VRF Consumer Added: ", perpetualMint);
-        console.log(
+        executeBatch(gnosisSafeAddress, true);
+
+        console2.log("Supra VRF Router Address: ", vrfRouter);
+        console2.log("Supra VRF Consumer Added: ", perpetualMint);
+        console2.log(
             "Supra VRF Subscription Funded: %s.%s ETH",
             envEthAmountToFundSubscription,
             ethAmountToFundSubscription % 1e18
         );
-
-        vm.stopBroadcast();
     }
 
     /// @notice attempts to read the saved address of the Core diamond contract, post-deployment
--- script/Base/post-deployment/configure-vrf-base.sh
+++ script/Base/post-deployment/configure-vrf-base.sh
@@ -4,7 +4,11 @@ set -e
 CHAIN_ID=8453
 CONFIGURATION_SCRIPT="01_configureVRFSubscription.s.sol"
 RPC_URL=$BASE_RPC_URL
+export CHAIN="base"
+export GNOSIS_SAFE="0x0000000000000000000000000000000000000000"
 export ETH_FUND_AMOUNT=20 # 20 ETH
+export PRIVATE_KEY=$DEPLOYER_KEY
+export WALLET_TYPE="local"
 
 # Check if DEPLOYER_KEY is set
 if [[ -z $DEPLOYER_KEY ]]; then
@@ -19,21 +23,21 @@ echo -e "Deployer Address: $DEPLOYER_ADDRESS\n"
 # Convert ETH_FUND_AMOUNT to Wei
 ETH_FUND_AMOUNT_WEI=$(cast to-wei $ETH_FUND_AMOUNT)
 
-# Get current ETH balance in Wei
-DEPLOYER_CURRENT_BALANCE_WEI=$(cast balance $DEPLOYER_ADDRESS)
+# Get current Gnosis Safe ETH balance in Wei
+GNOSIS_SAFE_CURRENT_BALANCE_WEI=$(cast balance $GNOSIS_SAFE)
 
 # Convert ETH balance from Wei to ETH base unit
-DEPLOYER_BALANCE_ETH_DEC=$(cast from-wei $DEPLOYER_BALANCE_LINK_WEI)
+GNOSIS_SAFE_BALANCE_ETH_DEC=$(cast from-wei $GNOSIS_SAFE_CURRENT_BALANCE_WEI)
 
-if [ $(echo "$DEPLOYER_CURRENT_BALANCE_WEI < $ETH_FUND_AMOUNT_WEI" | bc -l) -eq 1 ]; then
-  echo -e "Error: Deployer ETH balance is $DEPLOYER_BALANCE_ETH_DEC ETH, which is less than the specified ETH amount to fund the Supra VRF Subscription ($ETH_FUND_AMOUNT ETH).\n"
+if [ $(echo "$GNOSIS_SAFE_CURRENT_BALANCE_WEI < $ETH_FUND_AMOUNT_WEI" | bc -l) -eq 1 ]; then
+  echo -e "Error: Gnosis Safe ETH balance is $GNOSIS_SAFE_BALANCE_ETH_DEC ETH, which is less than the specified ETH amount to fund the Supra VRF Subscription ($ETH_FUND_AMOUNT ETH).\n"
   exit 1
 fi
 
-echo -e "Deployer ETH balance is $DEPLOYER_BALANCE_ETH_DEC ETH.\n"
+echo -e "Gnosis Safe ETH balance is $GNOSIS_SAFE_BALANCE_ETH_DEC ETH.\n"
 
 # Create broadcast directories for storing configuration data
 mkdir -p ./broadcast/${CONFIGURATION_SCRIPT}/$CHAIN_ID
 
 # Run forge scripts
-forge script script/Base/post-deployment/${CONFIGURATION_SCRIPT} --rpc-url $RPC_URL --broadcast
+forge script script/Base/post-deployment/${CONFIGURATION_SCRIPT} --rpc-url $RPC_URL --broadcast --ffi --sender $DEPLOYER_ADDRESS
